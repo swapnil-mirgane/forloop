862264a570ada1fa249ee2ce986ae348be9e98da
Swapnil Mirgane <swapnil.mirgane@corp.ola.com>
2022-07-26 16:30:19 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ForLoop></ForLoop>
+  <ForLoop/>
+
+</template>
+
+<script>
+import ForLoop from './components/ForLoop.vue'
+
+export default {
+    name: "App",
+    components: { ForLoop, }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ForLoop.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <table border="1px">
+    <thead><tr><th>Name</th><th>address</th><th>City</th></tr></thead>
+    <tbody>
+    <tr  v-for="items in student" :key="items"><td>{{items.sname}}</td> <td>{{items.address}}</td><td>{{items.city}}</td></tr>
+    </tbody></table>
+    <table border="1px">
+    <thead><tr><th>Name</th><th>address</th><th>City</th></tr></thead>
+    <tbody>
+    <tr  v-for="items of student" :key="items"><td>{{items.sname}}</td> <td>{{items.address}}</td><td>{{items.city}}</td></tr>
+    </tbody></table>
+</div>
+</template>
+<script >
+export default {
+    name:'FooLoop',
+    data() {
+        return {
+            student:[{sname:'swapnil',address:'pune',city:'pune'},
+            {sname:'madhuri',address:'pune',city:'pune'},
+            {sname:'kunal',address:'solapur',city:'solapur'},]
+        }
+    },
+
+}
+</script>
